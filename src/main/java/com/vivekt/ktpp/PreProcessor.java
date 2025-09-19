98f99793d8c39d5f7d571922e680aefdcce1ff8f
Vivek Lenovo <babloo32123@gmail.com>
2025-09-19 07:52:05 +0300

    Initial files

---
+++ src/main/java/com/vivekt/ktpp/PreProcessor.java
@@ -0,0 +1,7 @@
+package com.vivekt.ktpp;
+
+public class PreProcessor {
+    public static void main(String[] args) {
+        System.out.println("ktpp coming");
+    }
+}
---
+++ src/test/java/com/vivekt/ktpp/PreProcessorTest.java
@@ -0,0 +1,10 @@
+package com.vivekt.ktpp;
+
+import org.junit.jupiter.api.Test;
+
+public class PreProcessorTest {
+@Test
+    void test2test(){
+    System.out.println("tests coming soon.");
+}
+}
